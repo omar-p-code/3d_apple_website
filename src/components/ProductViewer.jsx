76cb2661a37694aafa4b43ecbf8ecbc6d4d9574b
omar <omaraldyb2020@gmail.com>
2026-02-14 06:26:16 +0200

    Initialize producut veiwer component

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import useMacBookStore from '../store'
+import clsx from 'clsx';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+
+function ProductViewer() {
+   const { color, scale, setColor, setScale } = useMacBookStore();
+
+   return (
+      <section id='product-viewer'>
+         <h2>Take a closer look.</h2>
+
+         <div className='controls'>
+            <p>MacBookPro {scale * 200}″ in silver / Space Black {color}</p>
+
+            <div className='flex-center gap-5 mt-5'>
+               <div className="color-control">
+                  <div
+                     onClick={() => setColor('#adb5bd')}
+                     className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
+                  />
+                  <div
+                     onClick={() => setColor('#2e2c2e')}
+                     className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')}
+                  />
+               </div>
+
+               <div className='size-control'>
+                  <div
+                     onClick={() => setScale(.06)}
+                     className={clsx(scale === .06 ? 'bg-white text-black' : 'bg-transparent')}
+                  >
+                     <p>12″</p>
+                  </div>
+                  <div
+                     onClick={() => setScale(.08)}
+                     className={clsx(scale === .08 ? 'bg-white text-black' : 'bg-transparent')}
+                  >
+                     <p>16″</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
+            <Box position={[0, 1.1, 0]} scale={10 * scale} material-color={color} />
+            <OrbitControls enableZoom={false} />
+         </Canvas>
+      </section>
+   )
+}
+
+export default ProductViewer
